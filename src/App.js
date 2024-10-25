3df9361bc4612073fd64416d5e741c34bb20a18b
Luiz Guilherme <luizsett7@gmail.com>
2024-10-24 21:48:48 -0300

    pages and components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <h1>Blog</h1>
+      <BrowserRouter>
+      <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <p>React Blog &copy; 2024</p>
+  </footer>
+}
+
+export default Footer
