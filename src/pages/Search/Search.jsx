5dc28d25e595795d918c6fadb1abe140ed7dc369
Luiz Guilherme <luizsett7@gmail.com>
2024-10-30 20:06:13 -0300

    Search form and Post page added

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,33 @@
+import styles from './Search.module.css'
+
+import { Link } from 'react-router-dom'
+
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useQuery } from "../../hooks/useQuery";
+import PostDetail from "../../components/PostDetail"
+
+const Search = () => {
+  const query = useQuery();
+  const search = query.get("q");
+
+  const { documents: posts} = useFetchDocuments("posts", search)
+
+  return (
+    <div className={styles.search_container}>
+        <h2>Search</h2>
+        <div>
+          {posts && posts.length === 0 && (
+            <>
+              <p>Posts not found</p>
+              <Link to="/" className="btn btn-dark">
+                Voltar
+              </Link>
+            </>
+          )}
+          {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        </div>
+    </div>
+  )
+}
+
+export default Search
